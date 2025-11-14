3609e87fe49fb51b78c57e6bd597d586a0a4ea88
freii ☘︎ <jamet.fa@obmontessori.edu.ph>
2025-11-14 09:03:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from pyscript import display
 
-#summary
-clubs = {"Guy1", "Guy2", "Guy3", "Guy4", "Guy5"}
-clubs2 = {"Guy2", "Guy5"}
-oneclub = {"Guy1", "Guy3"}
-clubtwo = {"Guy4"}
-oneclub = {"Guy1", "Guy3", "Guy4"}
-
-All_Clubs = {"Theresa", "Aimi", "BirthdayGuy", "Yuri", "Sylvia", "Monika"}
-Art_Club = {"Theresa", "Aimi", "BirthdayGuy"}
-Glee_Club = {"Yuri", "Sylvia", "Monika"}
-Both_Clubs = {"Sylvia", "Monika", "BirthdayGuy"}
+Art_Club = {"Theresa", "Aimi", "BirthdayGuy", "Sylvia", "Monika"}
+Glee_Club = {"Yuri", "Sylvia", "Monika", "BirthdayGuy"}
 
 display(f'Students who are in clubs:', Art_Club.union(Glee_Club), target="display")
-display(f'Students who are in the Art Club:', Art_Club.union(Both_Clubs), target="display")
-display(f'Students who are in the Glee Club:', Glee_Club.union(Both_Clubs), target="display")
-display(f'Students who are in one club:', All_Clubs.difference(Both_Clubs), target="display" )
-display(f'Students who are in both clubs:', Both_Clubs, target="display")
+display(f'Students who are in the Art Club:', Art_Club, target="display")
+display(f'Students who are in the Glee Club:', Glee_Club, target="display")
+display(f'Students who are in one club:', Art_Club.difference(Glee_Club),Glee_Club.difference(Art_Club), target="display" )
+display(f'Students who are in both clubs:', Art_Club.intersection(Glee_Club), target="display")
